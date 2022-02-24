572c282410ca4a7ae6e98c0fe872da40fc7f7e56
filipkro <filip.kronstrom@gmail.com>
2022-02-24 09:40:38 +0000

    some scripts

---
+++ scripts/rename-script.py
@@ -0,0 +1,29 @@
+''' as downloading using provided links is not working,
+    instead copy from ftp location. To do this:
+    First mount ftp.sra.ebi.ac.uk
+    then find location of this mount on your machine,
+    on ubuntu this should be something similar to base below,
+    Run this script in the same folder as E-MTAB-9786 file,
+    run with any python 3
+
+    then run resulting .sh file on local machine
+    - todo so, setup connection to server as bbt-remote
+
+    this is a hacky solution to something which shouldn't be a problem...
+'''
+
+import os
+file = open('E-MTAB-9786.sdrf.txt', 'r')
+#write = open('rename_ftps.sh', 'w')
+base = '/run/user/1000/gvfs/ftp:host=ftp.sra.ebi.ac.uk/vol1/fastq/ERR485/'
+for line in file:
+    splits = line.split('\t')
+    if splits[29][0] == 'f':
+        old_name = splits[29].split('/')[-1]
+        new_name = f"{splits[27].split('.')[-1]}.fastq"
+        
+        print(new_name)
+        #path = os.path.join(base, fp)
+        #write.write(f'scp {path} bbt-remote:data/ \n')
+#file.close()
+#write.close()
